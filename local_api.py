95d511df3e6850e753cbe793b31329ddc3ff40f0
Hayley <hgideo1@wgu.edu>
2026-05-12 09:54:30 -0500

    Completed local API requests

--- local_api.py
+++ local_api.py
@@ -2,13 +2,13 @@ import json
 
 import requests
 
-# TODO: send a GET using the URL http://127.0.0.1:8000
-r = None # Your code here
+# send a GET using the URL http://127.0.0.1:8000
+r = requests.get("http://127.0.0.1:8000")
 
-# TODO: print the status code
-# print()
-# TODO: print the welcome message
-# print()
+# print the status code
+print("GET status code:", r.status_code)
+# print the welcome message
+print("GET response:", r.json())
 
 
 
@@ -29,10 +29,13 @@ data = {
     "native-country": "United-States",
 }
 
-# TODO: send a POST using the data above
-r = None # Your code here
+# send a POST using the data above
+r = requests.post(
+    "http://127.0.0.1:8000/data/",
+    data=json.dumps(data)
+)
 
-# TODO: print the status code
-# print()
-# TODO: print the result
-# print()
+# print the status code
+print("POST status code:", r.status_code)
+# print the result
+print("POST response:", r.json())

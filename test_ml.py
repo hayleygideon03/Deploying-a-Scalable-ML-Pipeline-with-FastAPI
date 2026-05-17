3f1cb8a6568ca2d0753cbcb552b794f63b7023ba
Hayley <hgideo1@wgu.edu>
2026-05-16 19:38:28 -0500

    updated unit tests

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,66 @@
 import pytest
-# TODO: add necessary import
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
-    pass
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from ml.data import process_data
+from ml.model import train_model, inference, compute_model_metrics
+
+cat_features = [
+    "workclass",
+    "education",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native-country"
+]
+
+data = pd.read_csv("data/census.csv")
+
+train, test = train_test_split(data, test_size=0.20)
+
+X_train, y_train, encoder, lb = process_data(
+    train,
+    categorical_features=cat_features,
+    label="salary",
+    training=True
+)
+
+X_test, y_test, _, _ = process_data(
+    test,
+    categorical_features=cat_features,
+    label="salary",
+    training=False,
+    encoder=encoder,
+    lb=lb
+)
+
+model = train_model(X_train, y_train)
 
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_model_type():
     """
-    # add description for the second test
+    ensure model is using RandomForestClassifier
     """
-    # Your code here
-    pass
+   
+    assert isinstance(model, RandomForestClassifier)
 
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_inference_size():
     """
-    # add description for the third test
+    test that the inference returns the same number of predictions as number of rows passed in
     """
-    # Your code here
-    pass
+    preds = inference(model, X_test)
+
+    assert len(preds) == len(X_test)
+
+
+def test_prediction_values():
+    """
+    # Tests if predictions return only 0 or 1
+    """
+    preds = inference(model, X_test)
+
+    assert set(preds).issubset({0,1})
